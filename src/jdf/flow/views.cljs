c4d2dde194e32e565fd7e24f844ba5899ad32787
Jeremy Field <jdf@jdf.id.au>
2020-11-05 14:13:16 +1100

    styling, licence

--- src/jdf/flow/views.cljs
+++ src/jdf/flow/views.cljs
@@ -9,17 +9,17 @@
   (try (.toFixed v ({1 0, 0.1 1, 0.01 2} precision))
        (catch :default e (js/console.log v (string?  v)))))
 
-(defnc value [{:keys [id calcs element]}]
+(defnc value [{:keys [id calcs]}]
   (let [{:keys [precision unit label min max low high]} (model/parameters id)]
-    ($ (name element) {:class "value"} (fix (id calcs) precision))))
+    (d/div {:class ["value"]} (fix (id calcs) precision))))
 
 (defnc single [{:keys [id calcs]}]
   (let [{:keys [precision unit label]} (model/outputs id)]
     (d/div {:class "tile single"}
       (d/div {:class "header"}
-        (d/span {:class "id"} (name id))
-        (d/span {:class "unit"} unit))
-      ($ value {:id id :calcs calcs :element :div})
+        (d/div {:class "id"} (name id))
+        (d/div {:class "unit"} unit))
+      ($ value {:id id :calcs calcs})
       (d/div {:class "label"} label))))
 
 (defnc multi [{:keys [ids calcs]}]
@@ -28,17 +28,17 @@
       (for [id ids :let [{:keys [unit label]} (model/outputs id)]]
         (d/div {:class "row" :key id}
           ; use hier css selector to lay out same classes differently
-          (d/span {:class "id"} (name id))
-          ($ value {:id id :calcs calcs :element :span})
-          (d/span {:class "unit"} unit))))))
+          (d/div {:class "id"} (name id))
+          ($ value {:id id :calcs calcs})
+          (d/div {:class "unit"} unit))))))
           ; TODO where to put label?
 
 (defnc button [{:keys [selected set-selected id value]}]
   (let [{:keys [precision]} (model/inputs id)]
     (d/button {:class [(when selected "selected")]
                :onClick #(set-selected id)}
-      (d/span {:class "name"} (name id))
-      (d/span {:class "value"} (fix value precision)))))
+      (d/div {:class "name"} (name id))
+      (d/div {:class "value"} (fix value precision)))))
 
 (defnc root []
   (let [[state set-state] (hooks/use-state model/starting-values)
@@ -66,7 +66,7 @@
         (d/div {:class "slider-widget"}
           (d/div {:class "label"} label)
           (d/input {:type "range" :min min :max max :step precision
-                    :class "range" ; better css selector?
+                    :class "slider"
                     :value (selected state)
                     :onChange #(set-state ; range input returns string ugh
                                  (fn [s] (assoc s selected
